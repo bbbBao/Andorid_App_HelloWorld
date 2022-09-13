de90fb789cfb843e31b30ac1a33d538071f0e7f5
Tianyi Bao <tianyib831@gmail.com>
2022-09-13 14:55:32 -0400

    First Android app

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void buttonClick (View view) {
+        TextView txtHello = findViewById(R.id.txtHelloWorld);
+        txtHello.setText("Hello World.");
+    }
+
+
+}
